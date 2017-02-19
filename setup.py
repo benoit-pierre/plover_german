a561a4a71eed84796090e11ec9c02c8008b36c46
Benoit Pierre <benoit.pierre@gmail.com>
2017-02-19 15:41:58 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'plover_german',
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'German system for Plover',
     author = 'Foo Bar',
     author_email = 'foo.bar@foobar.com',
